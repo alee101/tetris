4acf4a26f6364d7862e5124760f25ab273e58dc7
albert <albert@blend.com>
2017-10-27 01:32:24 -0700

    nicer ui

--- src/cljs/tetris/views.cljs
+++ src/cljs/tetris/views.cljs
@@ -1,8 +1,25 @@
 (ns tetris.views
   (:require [re-frame.core :as re-frame]
+            [tetris.game :as game]
             [tetris.subs :as subs]))
+
+(defn cell [row col cell]
+  [:rect
+   {:height 1.0
+    :width 1.0
+    :stroke "black"
+    :stroke-width 0.05
+    :fill (if (game/empty-cell? cell) "black" "white")
+    :x col
+    :y row}])
 
 (defn tetris-app []
   (let [cur-board @(re-frame/subscribe [::subs/cur-board])]
-    [:div
-     [:pre (clojure.string/join "\n" (map str cur-board))]]))
+    (into
+     [:svg
+      {:height 400
+       :width 200
+       :view-box (clojure.string/join " " [0 0 game/board-width game/board-height])}]
+     (for [row (range game/board-height)
+           col (range game/board-width)]
+       [cell row col (get-in cur-board [row col])]))))
