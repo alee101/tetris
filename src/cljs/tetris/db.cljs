8895109f36cff76e0379f8016fedcf9b752fe80d
albert <albert@blend.com>
2017-12-02 15:00:18 -0800

    use spec

--- src/cljs/tetris/db.cljs
+++ src/cljs/tetris/db.cljs
@@ -1,5 +1,18 @@
 (ns tetris.db
-  (:require [tetris.game :as game]))
+  (:require [cljs.spec.alpha :as s]
+            [tetris.game :as game]))
+
+(s/def ::cell (set (range 8)))
+(s/def ::piece (s/coll-of (s/coll-of ::cell)))
+(s/def ::row (s/coll-of ::cell :count game/board-width))
+(s/def ::board (s/coll-of ::row :count game/board-height))
+
+(s/def ::cur-status #{:in-progress :paused :game-over})
+
+(s/def ::col-offset (s/and #(>= % 0) #(< % game/board-width)))
+(s/def ::row-offset (s/and #(>= % 0) #(< % game/board-height)))
+
+(s/def ::db (s/keys :req-un [::board ::piece ::cur-status ::row-offset ::col-offset]))
 
 (def default-db
   (game/new-game))
--- src/cljs/tetris/events.cljs
+++ src/cljs/tetris/events.cljs
@@ -1,24 +1,35 @@
 (ns tetris.events
-  (:require [re-frame.core :as re-frame]
+  (:require [cljs.spec.alpha :as s]
+            [re-frame.core :as re-frame]
             [tetris.db :as db]
             [tetris.game :as game]))
 
+(def check-spec-interceptor
+  (re-frame/after
+   (fn [db _]
+     (when-not (s/valid? ::db/db db)
+       (throw (ex-info (str "spec check failed: " (s/explain-str ::db/db db)) {}))))))
+
 (re-frame/reg-event-db
  ::initialize-db
+ check-spec-interceptor
  (fn [_ _]
    db/default-db))
 
 (re-frame/reg-event-db
  ::move
+ check-spec-interceptor
  (fn [db [_ action]]
    (game/move-if-valid db action)))
 
 (re-frame/reg-event-db
  ::gravitate
+ check-spec-interceptor
  (fn [db _]
    (game/gravitate db)))
 
 (re-frame/reg-event-db
  ::toggle-pause
+ check-spec-interceptor
  (fn [db _]
    (game/toggle-pause db)))
